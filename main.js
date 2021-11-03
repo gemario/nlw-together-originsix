46db888948fae9ec8cf752fcc406877c737709ab
Jorge Mario <gmario.sh@gmail.com>
2021-11-03 04:50:51 -0300

    Aprofundando os conceitos

--- main.js
+++ main.js
@@ -15,16 +15,17 @@ for (const link of links) {
   })
 }
 
-const header = document.querySelector('#header')
-const navHeigth = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeigth = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeigth) {
-    header.classList.add('scroll')
+   header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
+
 
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
@@ -47,7 +48,24 @@ scrollReveal.reveal(
    #about .image, #about .text,
    #services header, #services .card,
    #testimonials header, #testimonials .testimonials,
-   #contact .text, #contact .links
+   #contact .text, #contact .links,
+   footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
